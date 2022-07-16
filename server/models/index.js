71689b9af81e03843266fff5976f2d865414ed74
SabotMBT <104104069+SabotMBT@users.noreply.github.com>
2022-07-16 17:23:19 -0600

    basic seeding done

--- server/models/index.js
+++ server/models/index.js
@@ -4,28 +4,28 @@ const Inventory = require("./Inventory");
 const Order = require("./Order");
 const Category = require("./Category");
 
-Inventory.hasOne(Pricing, {
-  foreignKey: "inventory_id",
-});
+// Inventory.hasOne(Pricing, {
+//   foreignKey: "inventory_id",
+// });
 
-Pricing.belongsTo(Inventory, {
-  foreignKey: "inventory_id",
-});
+// Pricing.belongsTo(Inventory, {
+//   foreignKey: "inventory_id",
+// });
 
-Inventory.belongsTo(Users, {
-  foreignKey: "inventory_id",
-});
+// Inventory.belongsTo(Users, {
+//   foreignKey: "inventory_id",
+// });
 
-Order.hasMany(Inventory, {
-  foreignKey: "order_id",
-});
+// Order.hasMany(Inventory, {
+//   foreignKey: "order_id",
+// });
 
-Users.hasMany(Order, {
-  foreignKey: "order_id",
-});
+// Users.hasMany(Order, {
+//   foreignKey: "order_id",
+// });
 
-Order.hasOne(Users, {
-  foreignKey: "order_id",
-});
+// Order.hasOne(Users, {
+//   foreignKey: "order_id",
+// });
 
 module.exports = { Users, Pricing, Inventory, Order, Category };
