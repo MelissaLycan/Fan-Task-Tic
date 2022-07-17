cbf29eab1473f76c2462ab25951c982f82b17e9a
SabotMBT <104104069+SabotMBT@users.noreply.github.com>
2022-07-16 18:07:17 -0600

    merging

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -47,7 +47,7 @@ export default function Container() {
     <ApolloProvider client={client}>
       <Routes>
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/Band/:bandId" element={<Band />} />
+        <Route path="/Band/:bandId" element={<Band />} />
         <Route path="/User" element={<User />} />
         <Route path="/User/Band" element={<BandUser />} />
         <Route path="/User/Admin" element={<AdminUser />} />
@@ -55,7 +55,7 @@ export default function Container() {
         <Route path="/User/Band/Merchandise" element={<Merch />} />
         <Route path="/Band/:bandId/Shop/Cart" element={<Cart />} />
         <Route path="/Band/:bandId/Shop/:itemId" element={<ShopItem />} />
-        <Route path="/Band/:bandId/Shop" element={<Shop />} /> */}
+        <Route path="/Band/:bandId/Shop" element={<Shop />} />
       </Routes>
     </ApolloProvider>
   );
