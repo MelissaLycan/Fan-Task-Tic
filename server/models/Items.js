b6c117b994c6ddc30af459bf8a753fd532bb4ecb
SabotMBT <104104069+SabotMBT@users.noreply.github.com>
2022-07-18 10:45:40 -0600

    merging

---
+++ server/models/Items.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const itemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image_file: {
+    type: String,
+    required: true,
+  },
+  current_stock: {
+    type: Number,
+    required: false,
+    defaultValue: 0,
+  },
+  cost: {
+    type: Schema.Types.Decimal128,
+    required: true,
+  },
+  sales_price: {
+    type: Schema.Types.Decimal128,
+    required: true,
+  },
+  order_link: {
+    type: String,
+    required: false,
+  },
+});
+
+const Item = model("Item", itemSchema);
+module.exports = { Item, itemSchema };
