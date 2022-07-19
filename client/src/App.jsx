e21148dae817859e6945ca442de04d4f41d23034
Melissa Lycan <melissamlycan@gmail.com>
2022-07-19 00:10:35 -0600

    stashing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import { ApolloProvider } from "@apollo/client";
 import Container from "./components/Container";
 import Header from "./components/Header";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Band from "./components/pages/Band"
 
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { Events } from "./components/Events"
+// import { Spotify } from "./components/Spotify"
 import "./app.css";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Container />
+          <Header />
+          <Container />
     </div>
   );
 }
--- server/models/index.js
+++ server/models/index.js
@@ -3,23 +3,4 @@ const Item = require("./Items");
 const Order = require("./Order");
 const Category = require("./Category");
 
-<<<<<<< HEAD
-// Item.belongsTo(Users, {
-//   foreignKey: "item_id",
-// });
-
-// Order.hasMany(Item, {
-//   foreignKey: "order_id",
-// });
-
-// Users.hasMany(Order, {
-//   foreignKey: "order_id",
-// });
-
-// Order.hasOne(Users, {
-//   foreignKey: "order_id",
-// });
-
-=======
->>>>>>> 7b1fc8113a299347cc96d4028c28d5fdf85a7d1d
 module.exports = { Users, Item, Order, Category };
